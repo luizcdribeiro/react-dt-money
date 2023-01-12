edceb07c00f55699c5c890488d97cad93a5f6779
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2023-01-12 19:17:17 -0300

    Estrutura e css do projeto

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,43 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm/>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='40%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width='40%'>Lanche</td>
+              <td> 
+                <PriceHighLight variant="outcome">
+                  - R$ 60,00
+                </PriceHighLight></td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+
+    </div>
+  )
+}
